8012380e23724a69bef1e0a1b7f8cdab25b85882
OvelhaManca <vagner.matos47@gmail.com>
2024-04-27 00:04:04 -0300

    user and post model prisma

---
+++ prisma/migrations/20240427025718_post_migration/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "mediaUrl" TEXT,
+    "scheduleDate" DATETIME,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "publishedAt" DATETIME
+);
+INSERT INTO "new_Post" ("id", "title") SELECT "id", "title" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { PostModule } from './post/post.module';
 import { CronService } from './post/cron.service';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 
 @Module({
-  imports: [PostModule, UsersModule],
+  imports: [PostModule, UsersModule, AccountsModule],
   controllers: [],
   providers: [CronService, PrismaService],
 })
